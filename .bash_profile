f10ee72c1fabd78936c75218cac10800270384f9
gitAwa <onepieceawa@gmail.com>
2022-03-27 17:56:09 +0900

    add: files

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -sf ~/dotfiles/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/dotfiles/.bash_profile ~/.tmux.conf
